03829691ac477a94eef7cf563c9a4a449e8debaf
Don Alvin <alvindon41@gmail.com>
2023-09-13 21:33:27 +0300

    height

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,10 @@
-import {AiOutlineSearch} from 'react-icons/ai'
-import {HiMenuAlt4} from 'react-icons/hi'
 import Hero from './Hero'
 import Navbar from './Navbar'
 
 const Header = () => {
   
   return (
-    <header className=' h-full lg:h-[100vh]'>
+    <header className='h-[100vh]'>
         <Navbar />
         <Hero />
     </header>
